f8bf7631ab866860e6e7cc2718d213ddd91a6efd
kurtmix <kurtmix123@gmail.com>
2016-08-02 15:36:59 -0400

    Finished Moving Files From C9 Over

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#check").click(function() {
+       $('#check').fadeOut('fast');
+    });
+});
+$(document).ready(function(){
+    $("#tests").click(function() {
+       alert("Woot woot you got good grades.")
+    });
+});
+$(document).ready(function(){
+    $("#check").click(function() {
+       $('#check').fadeOut('fast');
+    });
+});
